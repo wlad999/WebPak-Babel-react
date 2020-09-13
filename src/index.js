f516ae0657677d36ebe1e97a73ee9729a3bde471
wlad999 <wlad25888@gmail.com>
2020-09-13 09:41:30 +0300

    installed WP, end ran

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Log from "./log";
+import Calc from "./calc";
+
+const calc = new Calc();
+const log = new Log();
+
+log.log(calc.add(1, 2, 3));
+
+//npm install --save-dev webpack webpack-cli
+
+//запуск
+//npx webpack
+//for prodaction mode
+
+//npx webpack --mode development
+//for development mode
