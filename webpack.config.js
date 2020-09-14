5c9fbeb316a6cbcf861c53c8f7441d747f50bf30
wlad999 <wlad25888@gmail.com>
2020-09-14 09:59:19 +0300

    loader for different file types

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,27 @@ module.exports = {
   mode: "development",
   module: {
     rules: [
+      //-----------------Loading images---------------------------
       {
-        test: /.\png$/,
+        test: /\.(png|jpeg|jpg|gif|ico)$/,
         use: [
           {
             options: {
               outputPath: "images",
               name: "[name] - [sha1:hash:7].[ext]",
+            },
+            loader: "file-loader",
+          },
+        ],
+      },
+      //----------------Loading fonts--------------------------
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          {
+            options: {
+              outputPath: "fonts",
+              name: "[name].[ext]",
             },
             loader: "file-loader",
           },
